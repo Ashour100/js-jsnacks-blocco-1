41f2ab078893ec54a1c2276aa80fb8a04e05c77c
Ashour100 <interinstitute5@gmail.com>
2022-03-02 12:18:40 +0100

    jsnack3.1 adv

--- jsnack3.1/main.js
+++ jsnack3.1/main.js
@@ -1,16 +1,38 @@
 /**
  *
  *
-    Crea un array vuoto e chiedi all'utente un numero da inserire nell'array.
-    Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 200.
+    crea due liste
+    entrambe devono arrivare almeno a 200
+    in una ci metti i numeri inseriti pari
+    e nell'altra i dispari
  *
  */
 
-let arr=[];
-let somma=0;
+let arr1=[];
+let arr2=[];
+let somma1=0;
+let somma2=0;
+let check1=0;
+let check2=0;
 let num=0;
-while(somma<200){
+while(check1==0&&check2==0){
     num=parseFloat(prompt("inserisci un numer"));
-    arr.push(num);
-    somma+=num;
-}console.log(arr);
+    if(num%2==0){
+        if(somma1<200){
+            arr1.push(num);
+            somma1+=num;
+        }
+        else
+            check1=1;
+    }
+    else{
+        if(somma2<200){
+        arr2.push(num);
+        somma2+=num;
+        }
+        else
+        check1=1;
+    }
+}
+console.log(arr1);
+console.log(arr2);
