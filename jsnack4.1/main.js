2cc102163211790e2eade4092aab29c0cd059fed
Ashour100 <interinstitute5@gmail.com>
2022-03-02 12:41:29 +0100

    jsnack4.1 Bonus2

--- jsnack4.1/main.js
+++ jsnack4.1/main.js
@@ -19,16 +19,43 @@
 
 
 // Bonus1
-let n,m;
+// let n,m;
+// n=parseInt(prompt("inserisci un n"));
+// m=parseInt(prompt("inserisci un m"));
+// let c=0;
+// while(n>c){
+//     let array=[];
+//     for(let i=0;i<m;i++){
+//         array[i]=Math.floor(Math.random() * 4532)+4;
+//     }
+//     console.log(array);
+//     c++;
+// }
+
+
+
+
+// Bonus2
+let n,m,l;
 n=parseInt(prompt("inserisci un n"));
 m=parseInt(prompt("inserisci un m"));
+l=parseInt(prompt("inserisci un m"));
+let somma=0;
 let c=0;
 while(n>c){
     let array=[];
     for(let i=0;i<m;i++){
         array[i]=Math.floor(Math.random() * 4532)+4;
+        somma+=array[i]
+        if(i=m-1){
+            if(!(somma>=l)){
+                array=[];
+                somma=0;
+            }
+        }
     }
     console.log(array);
     c++;
 }
+
 
