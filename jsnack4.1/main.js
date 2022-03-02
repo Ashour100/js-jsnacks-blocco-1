9363eeaeef57cdca4ee8733d70ceca6ce030b049
Ashour100 <interinstitute5@gmail.com>
2022-03-02 12:32:46 +0100

    jsnack4.1

---
+++ jsnack4.1/main.js
@@ -0,0 +1,18 @@
+/**
+    Fai inserire un numero, che chiameremo N, all'utente.
+    Genera N array, ognuno formato da 10 numeri casuali tra 4 e 4532.
+    Ogni volta che ne crei uno, stampalo a schermo.
+ */
+let n;
+n=parseInt(prompt("inserisci un numero"));
+let c=0;
+while(n>c){
+    let array=[];
+    for(let i=0;i<10;i++){
+        array[i]=Math.floor(Math.random() * 4532)+4;
+    }
+    console.log(array);
+    c++;
+}
+
+
